dec041ffc90c45755376cbf5e2b5a6f45200c545
Nauan Aires <nauancastro@alu.ufc.br>
2025-06-12 16:11:58 -0300

    atividade concluída

---
+++ app/src/main/java/com/example/tasksmanagerapp/model/Task.kt
@@ -0,0 +1,16 @@
+package com.example.tasksmanagerapp.model
+
+enum class TaskPriority {
+    BAIXA, MEDIA, ALTA
+}
+
+enum class TaskCategory {
+    TRABALHO, CASA, ESTUDOS
+}
+
+data class Task(
+    val name: String,
+    val isCompleted: Boolean = false,
+    val category: TaskCategory = TaskCategory.CASA,
+    val priority: TaskPriority = TaskPriority.MEDIA
+)
